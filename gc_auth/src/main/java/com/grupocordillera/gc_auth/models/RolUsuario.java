3e057f8ccc46f28f882dc8e6262526bbdff147e3
CamiProgram <camilovsprogram355@gmail.com>
2026-06-18 19:33:06 -0400

    del_dupe_cont

--- gc_auth/src/main/java/com/grupocordillera/gc_auth/models/RolUsuario.java
+++
@@ -1,8 +0,0 @@
-package com.grupocordillera.gc_auth.models;
-
-public enum RolUsuario {
-    GERENTE,   // Acceso total y reportes analíticos de Python
-    ADMIN,     // Gestión del sistema y usuarios
-    CAJERO,    // Punto de Venta / Cajas (Reemplaza al antiguo Vendedor)
-    BODEGUERO  // Logística e Inventario
-}
